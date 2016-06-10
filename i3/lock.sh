12fb83790babf88d9cd0059c08f60835a94c6eb9
Antoine Damhet <antoine.damhet@lse.epita.fr>
2016-06-10 12:55:36 +0200

    Added screen lock at $mod+k
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 scrot /tmp/screen.png
-convert /tmp/screen.png -blur 0x5 /tmp/blur.png
-i3lock -i /tmp/blur.png
-
+if [ "$1" = "screen" ]; then
+	i3lock -i /tmp/screen.png -f
+else
+	convert /tmp/screen.png -blur 0x5 /tmp/blur.png
+	i3lock -i /tmp/blur.png
+fi
 exit 0;
