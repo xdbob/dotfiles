aba362d4ea442edc4166167d098642c7001b58f5
Antoine Damhet <antoine.damhet@lse.epita.fr>
2016-06-13 14:16:42 +0200

    Updated i3pystatus for cortana.
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

--- i3/status.py
+++ i3/status.py
@@ -15,6 +15,10 @@ status.register("clock",
 # Affiche la charge du processeur
 status.register("load")
 
+# Show your CPU temperature, if you have a Intel CPU
+status.register("temp",
+        format="{temp:.0f}°C",)
+
 # Affiche la taille de /home
 # Format:
 # 42/128G [86G]
@@ -33,16 +37,16 @@ status.register("pulseaudio",
 #
 # This would also display a desktop notification (via dbus) if the percentage
 # goes below 5 percent while discharging. The block will also color RED.
-status.register("battery",
-    format="{status}/{consumption:.2f}W {percentage:.2f}% {remaining:%E%hh:%Mm}",
-    alert=True,
-    battery_ident="BAT1",
-    alert_percentage=5,
-    status={
-        "DIS": "↓",
-        "CHR": "↑",
-        "FULL": "=",
-    },)
+# status.register("battery",
+#    format="{status}/{consumption:.2f}W {percentage:.2f}% {remaining:%E%hh:%Mm}",
+#    alert=True,
+#    battery_ident="BAT1",
+#    alert_percentage=5,
+#    status={
+#        "DIS": "↓",
+#        "CHR": "↑",
+#        "FULL": "=",
+#    },)
 
 # Show if DHCPD is running
 status.register("runwatch",
@@ -55,7 +59,26 @@ status.register("runwatch",
 #
 # Note: requires both netifaces-py3 and basiciw
 status.register("network",
-    interface="wifi",
-    format_up="{essid} {quality:03.0f}%",)
+    interface="eno1",
+    format_up="{v4cidr}",)
+
+# Shows disk usage of /
+# Format:
+# 42/128G [86G]
+status.register("disk",
+    path="/home",
+    format="{used}/{total}G [{avail}G]",)
+
+# Shows mpd status
+# Format:
+# Cloud connected▶Reroute to Remain
+status.register("mpd",
+    format="{title}{status}{album}",
+    status={
+        "pause": "▷",
+        "play": "▶",
+        "stop": "◾",
+    },
+    host="liza")
 
 status.run()
