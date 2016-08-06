9702521cb47ae43a18bd711bb8b739d483a104cf
Antoine Damhet <antoine.damhet@lse.epita.fr>
2016-08-06 14:39:04 +0200

    Removed oh My Zsh.

---
+++ zsh/theme.zsh
@@ -0,0 +1,12 @@
+autoload -U colors && colors
+setopt auto_cd
+setopt multios
+setopt prompt_subst
+
+# Candy theme from oh-my-zsh
+PROMPT=$'%{$fg_bold[green]%}%n@%m %{$fg[blue]%}%D{[%X]} %{$reset_color%}%{$fg[white]%}[%~]%{$reset_color%} $(git_prompt_info)\
+%{$fg[blue]%}->%{$fg_bold[blue]%} %#%{$reset_color%} '
+GIT_PROMPT_PREFIX="%{$fg[green]%}["
+GIT_PROMPT_SUFFIX="]%{$reset_color%}"
+GIT_PROMPT_DIRTY=" %{$fg[red]%}*%{$fg[green]%}"
+GIT_PROMPT_CLEAN=""
