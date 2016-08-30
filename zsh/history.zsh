3b1b8b8e220152969a62c50d9e54fbff2aab2463
Antoine Damhet <antoine.damhet@lse.epita.fr>
2016-08-30 19:26:36 +0200

    Fix history
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,9 +1,11 @@
+SAVEHIST=$HISTSIZE
+
 # Show history
 case $HIST_STAMPS in
-	"mm/dd/yyyy") alias history='fc -fl 1' ;;
-	"dd.mm.yyyy") alias history='fc -El 1' ;;
-	"yyyy-mm-dd") alias history='fc -il 1' ;;
-	*) alias history='fc -l 1' ;;
+  "mm/dd/yyyy") alias history='fc -fl 1' ;;
+  "dd.mm.yyyy") alias history='fc -El 1' ;;
+  "yyyy-mm-dd") alias history='fc -il 1' ;;
+  *) alias history='fc -l 1' ;;
 esac
 
 setopt append_history
