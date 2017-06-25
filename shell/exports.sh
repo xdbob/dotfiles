661fc4d11c6e8489e952c671a038fdc55cdb57d5
Antoine Damhet <antoine.damhet@lse.epita.fr>
2017-06-25 18:25:27 +0200

    Add ccache support
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

--- shell/exports.sh
+++ shell/exports.sh
@@ -8,3 +8,8 @@ export HISTSIZE=$((2 ** 15 - 1))
 if which nproc &> /dev/null && [ $(nproc) -gt 1 ]; then
 	export MAKEFLAGS="-j$(($(nproc) - 1))"
 fi
+
+CCACHEDIR=/usr/lib/ccache/bin
+if [ -d $CCACHEDIR ]; then
+	export PATH=$CCACHEDIR:$PATH
+fi
