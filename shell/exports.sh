5ed68191b5bb6bdf4ed4e3692f9111ce5b13a724
Antoine Damhet <antoine.damhet@lse.epita.fr>
2017-06-25 18:26:54 +0200

    Add default URI for virsh
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

--- shell/exports.sh
+++ shell/exports.sh
@@ -4,6 +4,7 @@ export EDITOR='vim'
 export NNTPSERVER=news.epita.fr
 export HISTFILE=$HOME/.history
 export HISTSIZE=$((2 ** 15 - 1))
+export VIRSH_DEFAULT_CONNECT_URI=qemu:///system
 
 if which nproc &> /dev/null && [ $(nproc) -gt 1 ]; then
 	export MAKEFLAGS="-j$(($(nproc) - 1))"
