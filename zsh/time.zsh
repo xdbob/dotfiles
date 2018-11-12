cc658c8fb309313035122a029596e74a3fa6f9d4
Antoine Damhet <antoine.damhet@lse.epita.fr>
2018-11-12 19:41:32 +0100

    zsh: add time.zsh (show time of long running commands)
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

---
+++ zsh/time.zsh
@@ -0,0 +1,32 @@
+# Imported from https://github.com/popstas/zsh-command-time, any and all
+# copyrights of the original file apply here.
+
+_command_time_preexec() {
+  timer=${timer:-$SECONDS}
+  ZSH_COMMAND_TIME_MSG=${ZSH_COMMAND_TIME_MSG-"Time: %s"}
+  ZSH_COMMAND_TIME_COLOR=${ZSH_COMMAND_TIME_COLOR-"white"}
+  export ZSH_COMMAND_TIME=""
+}
+
+_command_time_precmd() {
+  if [ $timer ]; then
+    timer_show=$(($SECONDS - $timer))
+    if [ -n "$TTY" ] && [ $timer_show -ge ${ZSH_COMMAND_TIME_MIN_SECONDS:-3} ]; then
+      export ZSH_COMMAND_TIME="$timer_show"
+      if [ ! -z ${ZSH_COMMAND_TIME_MSG} ]; then
+        zsh_command_time
+      fi
+    fi
+    unset timer
+  fi
+}
+
+zsh_command_time() {
+  if [ -n "$ZSH_COMMAND_TIME" ]; then
+    timer_show=$(printf '%dh:%02dm:%02ds\n' $(($ZSH_COMMAND_TIME/3600)) $(($ZSH_COMMAND_TIME%3600/60)) $(($ZSH_COMMAND_TIME%60)))
+    print -P '%F{$ZSH_COMMAND_TIME_COLOR}`printf "${ZSH_COMMAND_TIME_MSG}\n" "$timer_show"`%f'
+  fi
+}
+
+precmd_functions+=(_command_time_precmd)
+preexec_functions+=(_command_time_preexec)
